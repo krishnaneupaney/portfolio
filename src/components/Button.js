af2d0a8a63ee56141c9ade83f93c4e43db8c2ed0
krishnaneupaney <krishnaneupaney000@gmail.com>
2021-06-09 21:16:17 -0400

    about me

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,7 +18,7 @@ export const Button = ({
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return (
-        <Link to='/contact' className='btn-mobile'>
+        <Link to='/about' className='btn-mobile'>
             <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}
             type={type}
