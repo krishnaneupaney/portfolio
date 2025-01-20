45e833f6aea3ca625f39eed6e0d9cfdfe69b328c
Krishna Neupaney <79564250+krishnaneupaney@users.noreply.github.com>
2025-01-20 14:17:41 -0500

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,17 +8,13 @@ function About() {
             <div className='studently'>
                 <h1> Hello there! Krishna Here! 🤓</h1>
                 <br />
-                <h3>I'm a Full-Stack Software Developer with keen interest on design and architecture. 
-                    I love solving problems with code and building applications that solves problems in the society in various Languages  
-                   using different technologies.
+                <h3> I love solving problems with code and building applications that solves problems in the society.
                     <br />
                     <br />
-                    When I'm not coding or learning, you can find me on long run or reading books. 
+                    When I'm not coding or learning, you can find me reading books. 
                 </h3>
                 <br />
-                <h3>Some of my favorites books are:  The Law of Human Nature by Robert Green.
-                    Benjamin Franklin by Walter Issacson
-                    The Power of Now by Eckhart Tolle
+                <h3>
                 
                     
                 </h3>
@@ -27,19 +23,6 @@ function About() {
             <h2>Languages and Tools I work with </h2>
             <br />
 
-                <a href="https://python.org" target="_blank"><img src="https://www.python.org/static/community_logos/python-logo-inkscape.svg" alt="python" width="40" height="40" /></a>
-                <a href="https://javascript.com" target="_blank"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40"/> </a>
-                <a href="https://getbootstrap.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a>
-                <a href="https://www.w3schools.com/css/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a>
-                <a href="https://www.djangoproject.com/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/django/django-original.svg" alt="django" width="40" height="40"/> </a> <a href="https://expressjs.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> </a> 
-                <a href="https://git-scm.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://heroku.com" target="_blank"> <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="heroku" width="40" height="40"/> </a>
-                <a href="https://www.w3.org/html/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a>
-                <a href="https://nodejs.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://www.postgresql.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> 
-                <a href="https://angularjs" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original-wordmark.svg" alt="angularjs" width="40" height="40"/></a>
-                <a href="https://docker.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg" alt="docker" width="40" height="40" /></a>
-                <a href="http://mongodb.com" targer="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg" alt="mongo" width="40" height="40" /></a>
-
-
             <br />
             <br />
             <h2>Connect With me: </h2>
@@ -47,14 +30,7 @@ function About() {
             <p align="left">
         
 <div className="lmao">
-<a href="https://github.com/krishnaneupaney" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg" alt="krishnaneupaney" height="30" width="40" /></a>
-<a href="https://codepen.io/krishna" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/codepen.svg" alt="esin87" height="30" width="40" /></a>
-<a href="https://dev.to/krishnaneupaney" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/dev-dot-to.svg" alt="krishnaneupaney" height="30" width="40" /></a>
-<a href="https://linkedin.com/in/krishnaneupaney" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg" alt="krishnaneupaney" height="30" width="40" /></a>
-<a href="https://codesandbox.com/krishnaneupaney" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/codesandbox.svg" alt="krishnaneupaney" height="30" width="40" /></a>
-<a href="https://instagram.com/krishnaneupaney" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg" alt="krishnaneupaney" height="30" width="40" /></a>
-<a href="https://facebook.com/krishnaneupaney" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/facebook.svg" alt="krishnaneupaney" height="30" width="40" /></a>
-</div>
+
 </p>
 <br />
 <h3>Email: krishnaneupaney000@gmail.com</h3>
