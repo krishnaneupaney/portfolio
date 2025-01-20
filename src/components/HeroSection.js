df58990a954709728c1b6ac66967097511dc75e0
Krishna Neupaney <79564250+krishnaneupaney@users.noreply.github.com>
2025-01-20 14:19:58 -0500

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,22 +8,8 @@ function HeroSection() {
         <div className='hero-container'>
             <video src='/videos/video-2.mp4' autoPlay loop muted />
             <h1>Welcome To My Portfolio</h1> 
-            <p>Check out my work here and</p>  
-            <p>feel free to contact me with job offers.</p> 
-            <p>Most of the time, I accept it</p> 
-            <div className='hero-btns'>
-                <Button
-                    className='btns'
-                    buttonStyle='btn--primary'
-                    buttonSize='btn--large'>
-                       But Who Am I?
+       
                 </Button>
-                {/* <Button
-                    className='btns'
-                    buttonStyle='btn--primary'
-                    buttonSize='btn--large'>
-                       WATCH MY INTRO <i className='far fa-play-circle' />
-                </Button> */}
             </div>      
         </div>
     )
